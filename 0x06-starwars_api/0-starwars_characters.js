41f67bfd0ab13ab944e9a0b28c7c73d4e5aa8a6e
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-29 16:53:36 +0300

    wrap request in Promise to preserve character fetch order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,28 @@
-#!/usr/bin/node
+// #!/usr/bin/node
 
-const request = require('request');
+// const request = require('request');
 
-function requestCharacter (urlChar) {
-  return new Promise((resolve, reject) => {
-    request({ url: urlChar, json: true }, (error, response, body) => {
-      if (error) reject(error);
-      resolve(body);
-    });
-  });
-}
+// function requestCharacter (urlChar) {
+//   return new Promise((resolve, reject) => {
+//     request({ url: urlChar, json: true }, (error, response, body) => {
+//       if (error) reject(error);
+//       resolve(body);
+//     });
+//   });
+// }
 
-const movieId = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+// const movieId = process.argv[2];
+// const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-request({ url, json: true }, async (error, response, body) => {
-  if (error) console.log(error);
-  const film = body;
-  const characters = film.characters;
-  for (const urlChar of characters) {
-    try {
-      const charName = await requestCharacter(urlChar);
-      console.log(charName.name);
-    } catch (error) { console.log('Error fetching : ', error); }
-  }
-});
+// request({ url, json: true }, async (error, response, body) => {
+//   if (error) console.log(error);
+//   const film = body;
+//   const characters = film.characters;
+//   for (const urlChar of characters) {
+//     try {
+//       const charName = await requestCharacter(urlChar);
+//       console.log(charName.name);
+//     } catch (error) { console.log('Error fetching : ', error); }
+//   }
+// });
+			console.log(dsfd)
