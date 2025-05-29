dc065c9731fc1383223d1b48d4bdc9ea7eacf4cb
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-29 16:49:05 +0300

    wrap request in Promise to preserve character fetch order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,15 +2,24 @@
 
 const request = require('request');
 
+function requestCharacter (urlChar) {
+  return new Promise((resolve, reject) => {
+    request({ url: urlChar, json: true }, (error, response, body) => {
+      if (error) reject(error);
+      resolve(body);
+    });
+  });
+}
+
 const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
-request({ url, json: true }, (error, response, body) => {
+
+request({ url, json: true }, async (error, response, body) => {
   if (error) console.log(error);
-  const characters = body.characters;
+  const film = body;
+  const characters = film.characters;
   for (const urlChar of characters) {
-    request({ url: urlChar, json: true }, (error, response, body) => {
-      if (error) console.log(error);
-      console.log(body.name);
-    });
+    const charName = await requestCharacter(urlChar);
+    console.log(charName.name);
   }
 });
